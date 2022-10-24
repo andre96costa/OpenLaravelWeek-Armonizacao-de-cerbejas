83baac147500cad68ca440f7543946bc244b6989
Andre Costa <andre2396costa@gmail.com>
2022-10-23 21:41:10 -0300

    Projeto completo

---
+++ app/Http/Controllers/ExportController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Export;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Inertia\Inertia;
+
+class ExportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $exports = Export::paginate(15);
+
+        return Inertia::render('Reports', [
+            'exports' => $exports
+        ]);
+    }
+
+    public function show($export)
+    {
+        $export = Export::find($export);
+        //dd($export->file_name);
+        return Storage::download($export->file_name);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Export  $export
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($export)
+    {
+        $export = Export::find($export);
+        if ($export) {
+            Storage::delete($export->file_name);
+            $export->delete();
+        }
+        
+        return redirect()->back()->with('success', 'Seu arquivo foi removido com sucesso!');
+    }
+}
---
+++ config/punkapi.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'url' => env('PUNK_API_BASE_URL', 'https://api.punkapi.com/v2')
+];
